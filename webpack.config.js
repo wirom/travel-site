921fad06d6042253f01d26cdaa1d18b4460f2758
William Romero <wirom@gmail.com>
2020-06-28 07:21:32 -0400

    Hero block ended

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const currentTask = process.env.npm_lifecycle_event;
+let postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-nested"),
+  require("postcss-simple-vars"),
+  require("autoprefixer"),
+];
+let cssConfig = {
+  test: /\.css$/,
+  use: [
+    "css-loader",
+    { loader: "postcss-loader", options: { plugins: postCSSPlugins } },
+  ],
+};
+
+let config = {
+  entry: "./app/assets/scripts/App.js",
+  module: {
+    rules: [cssConfig],
+  },
+};
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader");
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  };
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("/app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    port: 3000,
+    host: "0.0.0.0",
+  };
+}
+
+if (currentTask == "build") {
+}
+
+module.exports = config;
