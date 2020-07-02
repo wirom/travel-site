44b4e942a67782150d2953a0e98ec29f8a5a61b2
William Romero <wirom@gmail.com>
2020-07-01 23:30:40 -0400

    final

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,29 @@
-import("../styles/styles.css");
-import MobileMenu from "./modules/MobileMenu.js";
-import RevealOnScroll from "./modules/RevealOnScroll";
+import '../styles/styles.css'
+import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
-let mobileMenu = new MobileMenu();
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
+let modal
 
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,56 +1,54 @@
-import throttle from "lodash/throttle";
-import debounce from "lodash/debounce";
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
 
 class RevealOnScroll {
   constructor(els, thresholdPercent) {
-    this.thresholdPercent = thresholdPercent;
-    this.itemsToReveal = els;
-    this.browserHeight = window.innerHeight;
-    this.hideInitially();
-    this.scrollThrottle = throttle(this.callCaller, 200).bind(this);
-    this.events();
+    this.thresholdPercent = thresholdPercent
+    this.itemsToReveal = els
+    this.browserHeight = window.innerHeight
+    this.hideInitially()
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+    this.events()
   }
 
   events() {
-    window.addEventListener("scroll", this.scrollThrottle);
-    window.addEventListener(
-      "resize",
-      debounce(() => {
-        this.browserHeight = window.innerHeight;
-      }, 333)
-    );
+    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener("resize", debounce(() => {
+      console.log("Resize just ran")
+      this.browserHeight = window.innerHeight
+    }, 333))
   }
 
-  callCaller() {
-    this.itemsToReveal.forEach((el) => {
+  calcCaller() {
+    console.log("Scroll function ran")
+    this.itemsToReveal.forEach(el => {
       if (el.isRevealed == false) {
-        this.calculateIfScrollTo(el);
+        this.calculateIfScrolledTo(el)
       }
-    });
+    })
   }
 
-  calculateIfScrollTo(el) {
-    //console.log(el.getBoundingClientRect().y);
+  calculateIfScrolledTo(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop) {
-      let scrollPercent =
-        (el.getBoundingClientRect().y / this.browserHeight) * 100;
+      console.log("Element was calculated")
+      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
       if (scrollPercent < this.thresholdPercent) {
-        el.classList.add("reveal-item--is-visible");
-        el.isRevealed = true;
+        el.classList.add("reveal-item--is-visible")
+        el.isRevealed = true
         if (el.isLastItem) {
-          window.removeEventListener("scroll", this.scrollThrottle);
+          window.removeEventListener("scroll", this.scrollThrottle)
         }
       }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach((el) => {
-      el.classList.add("reveal-item");
-      el.isRevealed = false;
-    });
-    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item")
+      el.isRevealed = false
+    })
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
   }
 }
 
-export default RevealOnScroll;
+export default RevealOnScroll
